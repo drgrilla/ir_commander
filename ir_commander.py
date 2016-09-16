ce3876793997729b9ccf8b622f4da2e434cef328
drgrilla <socratesg@gmail.com>
2016-09-15 17:27:26 -0700

    Implementing turn_ps4_on
    Implementing turn_ps4_on on by running ps4-waker. 
    Requires ps4-waker to be setup first.
    
    Feel free to reject these changes. ;-)

--- ir_commander.py
+++ ir_commander.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 import socket
 import time
-
-if __name__ == '__main__':
-    pass
+import subprocess
 
 #configure lightwave server settings
 LW_UDP_IP = "192.168.0.10"
@@ -22,6 +19,7 @@ def print_response(desc):
 	print("")
 	return
 	
+
 def send_udp(msg, description):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet and UDP
     print_response("Lightwave command: " + description + " - \"" + msg + "\"")
@@ -37,9 +35,18 @@ def send_lightwave_command(acomm,description):
     send_udp(msg,description)
     return
     
+
 def reboot_Pi():
     command = "/usr/bin/sudo /sbin/shutdown -r now"
-    import subprocess
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    print(output)
+    return
+    
+    
+def turn_PS4_On():
+    # Setup ps4-waker before https://www.npmjs.com/package/ps4-waker
+    command = "ps4-waker"
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
     print(output)
@@ -49,35 +56,38 @@ def reboot_Pi():
 #######################################
 ######## Message handling loop ########
 #######################################
+def main():
+  aCommand = ""
+  while aCommand != "0":
+      aCommand = input("Command? ")
+      if aCommand == "1": 
+          send_lightwave_command("R2FmP1","TV Time")
+      elif aCommand == "2": 
+          send_lightwave_command("R2Fa", "All off")
+      elif aCommand == "3":  
+          send_lightwave_command("R2FmP3","All on")
+      elif aCommand == "4":  
+          send_lightwave_command("R2FmP2","Dim room")
+      elif aCommand == "5": #low light for pause
+          send_lightwave_command("R2D3FdP2","Main Sofas 5%")
+    	  time.sleep(0.2)
+    	  send_lightwave_command("R2D4FdP2","Main Middle 5%")
+      elif aCommand == "6":
+          send_lightwave_command("R2D3F0","Main Sofas Off")
+    	  time.sleep(0.2)
+    	  send_lightwave_command("R2D4F0","Main Middle Off")
+      elif aCommand == "7":
+          lock_light_changes = True
+      elif aCommand == "8":
+      	  lock_light_changes = False
+      elif aCommand == "101":
+    	  turn_PS4_On()
+      elif aCommand == "999":
+          reboot_Pi()
+      elif aCommand == "register_lw": 
+          send_lightwave_command("F*p.", "LightwaveRF register command")
+  print("Finished!")
 
-aCommand = ""
-while aCommand != "0":
-    aCommand = input("Command? ")
-    if aCommand == "1": 
-        send_lightwave_command("R2FmP1","TV Time")
-    elif aCommand == "2": 
-        send_lightwave_command("R2Fa", "All off")
-    elif aCommand == "3":  
-        send_lightwave_command("R2FmP3","All on")
-    elif aCommand == "4":  
-        send_lightwave_command("R2FmP2","Dim room")
-    elif aCommand == "5": #low light for pause
-    	send_lightwave_command("R2D3FdP2","Main Sofas 5%")
-    	time.sleep(0.2)
-    	send_lightwave_command("R2D4FdP2","Main Middle 5%")
-    elif aCommand == "6":
-    	send_lightwave_command("R2D3F0","Main Sofas Off")
-    	time.sleep(0.2)
-    	send_lightwave_command("R2D4F0","Main Middle Off")
-    elif aCommand == "7":
-    	lock_light_changes = True
-    elif aCommand == "8":
-    	lock_light_changes = False
-    #elif aCommand == "101":
-    	#turn_PS4_On()
-    elif aCommand == "999":
-    	reboot_Pi()
-    elif aCommand == "register_lw": 
-        send_lightwave_command("F*p.", "LightwaveRF register command")
 
-print("Finished!")
+if __name__ == '__main__':
+    main()
